55b92884b0e4685d246f170627689bf4220cea0d
HORIE Tetsuya <tetsuya.ettem@gmail.com>
2018-09-14 17:20:41 +0900

    3ステップ目のfxadd1を実装

---
+++ 03_UnaryPrimitives/compiler.scm
@@ -0,0 +1,138 @@
+(import (rnrs hashtables))
+
+;;;; ユーティリティ関数
+
+;;; 書式と引数を取って表示し、改行を付け加えます。
+;; 例: (emit "addi t0, t0, ~s" 1)
+;; addi t0, t0, 1
+;; が表示されます。
+(define (emit . args)
+  (apply format #t args)
+  (newline))
+
+;;;; オブジェクト型情報定義: タグ付きポインタ表現を使う
+
+;;; 整数: 下位2ビットが00、上位30ビットが符号付き整数となっている
+(define fxshift 2)			; 整数変換シフト量
+(define fxmask #x03)			; 整数判定ビットマスク(ANDを取って0なら整数オブジェクト)
+
+;;; boolean: 
+(define bool_f #x2f)			; #fの数値表現 
+(define bool_t #x6f)			; #t
+(define boolmask #xbf)			; boolean判定ビットマスク(ANDを取ってis_boolならbooleanオブジェクト)
+(define is_bool #x2f)			; 
+
+;;; 空リスト:
+(define empty_list #x3f)		; 
+
+;;; 文字: 下位8ビットが00001111。ASCIIコードを8ビットシフトして、0x0fとORを取る。
+(define charmask #xff)			; char判定ビットマスク(ANDを取って、chartagならchar)
+(define chartag #x0f)			; charタグ
+(define charshift 8)			; char変換シフト量
+
+(define wordsize 4)			; 32bit(4バイト)
+
+(define fixnum-bits (- (* wordsize 8) fxshift))
+(define fxlower (- (expt 2 (- fixnum-bits 1))))
+(define fxupper (- (expt 2 (- fixnum-bits 1))
+		   1))
+(define (fixnum? x)
+  (and (integer? x) (exact? x) (<= fxlower x fxupper)))
+
+
+;;;; 即値関連関数
+
+;;; 即値かどうかを返します。
+(define (immediate? x)
+  (or (fixnum? x) (boolean? x) (char? x) (null? x)))
+
+;;; Schemeの即値から、アセンブリ言語でのオブジェクト表現を返します。
+(define (immediate-rep x)
+  (cond
+   ((fixnum? x) (ash x fxshift))
+   ((eq? x #f) bool_f)
+   ((eq? x #t) bool_t)
+   ((char? x) (logior (ash (char->integer x) charshift) chartag))
+   ((null? x) empty_list)
+   (else (error "invalid immediate"))))
+
+;;; 即値表現から、アセンブリ言語を出力します。
+(define (emit-immediate expr)
+  (emit "	li a0, ~s" (immediate-rep expr)))
+
+;;;; グローバル・プロバティ
+(define *prop* (make-eq-hashtable))
+
+(define (getprop x property)
+  (hashtable-ref
+   (hashtable-ref *prop* x #f)
+   property #f))
+
+(define (putprop x property val)
+  (let ((entry (hashtable-ref *prop* x #f)))
+    (if entry
+	(hashtable-set! entry property val)
+	(hashtable-set! *prop* 
+			x
+			(let ((entry (make-eq-hashtable)))
+			  (hashtable-set! entry property val)
+			  entry)))))
+
+;;;; プリミティブ関連
+
+;;; プリミティブ定義(*porp*にプリミティブ関連の情報を追加)
+;; 例: (define-primitive (fxadd1 arg)
+;;       出力内容 ...)
+(define-syntax define-primitive
+  (syntax-rules ()
+    ((_ (prime-name arg* ...) body body* ...)
+     (begin
+       (putprop 'prime-name '*is-prime* #t)
+       (putprop 'prime-name '*arg-count
+		(length '(arg* ...)))
+       (putprop 'prime-name '*emmiter*
+		(lambda (arg* ...)
+		  body body* ...))))))
+
+;;; 引数が基本演算かどうかを返します。
+; xは、add1のようにシンボルで、*is-prime*が#tにセットされている必要がある
+(define (primitive? x)
+  (and (symbol? x) (getprop x '*is-prime*)))
+
+(define (primitive-emitter x)
+  (or (getprop x '*emmiter*) (error "primitive-emitter: not exist emmiter")))
+
+;;;; 単項演算関連
+
+;;; 単項演算呼び出し(単項演算処理)かどうかを返します。
+;; 単項演算呼び出しは(op arg)の形式なので、最初はpairで、carがprimitive?がtrueを返すものでなければならない。
+(define (primcall? expr)
+  (and (pair? expr) (primitive? (car expr))))
+
+(define (emit-primcall expr)
+  (let ((prim (car expr))
+	(args (cdr expr)))
+    (apply (primitive-emitter prim) args)))
+
+;;; 引数に1を加えた値を返します
+(define-primitive (fxadd1 arg)
+  (emit-expr arg)
+  (emit "	addi a0, a0, ~s" (immediate-rep 1)))
+
+;;;; コンパイラ・メイン処理
+
+(define (emit-expr expr)
+  (cond
+   ((immediate? expr) (emit-immediate expr))
+   ((primcall? expr) (emit-primcall expr))
+   (else (error "imvalid expr"))))
+
+(define (emit-program expr)
+  (emit "	.text")
+  (emit "	.globl scheme_entry")
+  (emit "	.type scheme_entry, @function")
+  (emit "scheme_entry:")
+  (emit-expr expr)
+  (emit "	ret"))
+
+(emit-program '(fxadd1 3))
